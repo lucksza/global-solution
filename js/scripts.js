fc050e572aebe8e6ea2ea026bd2b2f7210a5df0b
lucksza <luccasaraivaborges@gmail.com>
2024-06-03 15:03:55 -0300

    seção Tecnologia Utilizada

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
+    const loginButton = document.getElementById('login-button');
+    const loginModal = document.getElementById('login-modal');
+    const closeButton = document.querySelector('.close-button');
+    const loginForm = document.getElementById('login-form');
 
     window.addEventListener('load', () => {
         sections.forEach(section => {
@@ -18,5 +22,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 behavior: 'smooth'
             });
         });
+    });
+
+    loginButton.addEventListener('click', () => {
+        loginModal.style.display = 'block';
+    });
+
+    closeButton.addEventListener('click', () => {
+        loginModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == loginModal) {
+            loginModal.style.display = 'none';
+        }
+    });
+
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        if (username === 'Lucca' && password === '123') {
+            alert('Login bem-sucedido!');
+            loginModal.style.display = 'none';
+        } else {
+            alert('Usuário ou senha incorretos.');
+        }
     });
 });
