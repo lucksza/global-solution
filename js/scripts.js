cec4d1350e81dcda169f36681e541443c70eba5e
lucksza <luccasaraivaborges@gmail.com>
2024-06-03 14:27:35 -0300

    seção Desafio Ambiental dos Oceanos

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
 
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-        hamburger.classList.toggle('active');
+    window.addEventListener('load', () => {
+        sections.forEach(section => {
+            section.classList.add('visible');
+        });
+    });
+
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - 70,
+                behavior: 'smooth'
+            });
+        });
     });
 });
